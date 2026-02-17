91f155506f75eb8167d3057b48fa52a118a88f34
Charles Baker <ch83baker@gmail.com>
2026-02-16 20:01:36 -0500

    Cleaned source dataframe, more metadata

--- src/data/systems_initializer.py
+++ src/data/systems_initializer.py
@@ -8,9 +8,10 @@ import pyarrow.parquet as pq
 from pathlib import Path
 import os
 import boto3
-import botocore
 from botocore.handlers import disable_signing
 import time
+import datetime
+import json
 
 # prepare for future pandas 3.0 usage
 pd.options.mode.copy_on_write = True
@@ -19,56 +20,113 @@ s3 = boto3.resource("s3")
 s3.meta.client.meta.events.register("choose-signer.s3.*", disable_signing)
 bucket = s3.Bucket("oedi-data-lake")
 
-# initialize downloads documentation
-downloads_cols = ['Filename', 'Source', 'Access Time']
-downloads_list = []
-downloads_path = Path('../../data_inventory.csv')
-if downloads_path.is_file():
-    downloads_df = pd.read_csv(downloads_path)
-else:
-    downloads_df = pd.DataFrame(data=None, columns=downloads_cols)
-
 
 def downloader(path_to_dir_local: str, path_to_dir_online: str,
-               warn_empty=False):
+               warn_empty=False, is_specific_file_type=False,
+               specific_file_type=''):
     '''Download a file or collection of files from the
     OEDI PVDAQ Data Lake.
-    More granular control than the pvdaq_access package.'''
-    global bucket, downloads_list
+    More granular control than the pvdaq_access package,
+    and included some logging.
+
+    Parameters
+    ------------
+    path_to_dir_local: str
+        A string representing the desired path to the file storage
+        on the local system.  Must be a valid directory [end in / or \\]
+    path_to_dir_online: str
+        A string directing the prefix of the filenames of the files
+        to access online.
+        Despite the name, this does not need to be a directory.
+    warn_empty: bool
+        Print if there are no items to download.
+    is_specific_file_type: bool
+        Print if you want to restrict to a particular file_type
+    specific_file_type: str
+        The specific file type you want.
+    '''
+    global bucket
+    downloads_list = []
     if path_to_dir_local[-1] != '/' and path_to_dir_local[-1] != '\\':
         raise ValueError('Local path does not end in "/" or "\\",'
-                         + 'is not a possible directory!')
+                         + ' and hence is not a possible directory!')
     my_local_dir = Path(path_to_dir_local)
     if not my_local_dir.is_dir():
         my_local_dir.mkdir()
     objects = bucket.objects.filter(
         Prefix=path_to_dir_online
     )
+    # sometimes objects goofs and gives a no-continuation prefix
+    # to the online directory/filepath
+    # in addition to the other objects, so some workarounds
+    # are necessary.
     if len(list(objects)) == 0:
         if warn_empty:
             print('No such files!')
+        return False
     else:
         for obj in objects:
             # horrible mix of os.path and pathlib.Path, but it works
-            online_file_path = Path(
+            file_path = Path(
                 os.path.join(
                     my_local_dir, os.path.basename(obj.key)
                 )
             )
-            # avoid re-downloading file
-            # may have to delete this check if multiprocessing necessary
-            if not online_file_path.is_file():
-                download_time = time.time()
-                bucket.download_file(
-                    obj.key, online_file_path
+            # Sometimes filter messes up and just gives us a directory back.
+            # if it's the same as the starting directory, ok,
+            # harmless error, skip it
+            # if it's not the same as the starting directory, flag it.
+            if file_path.is_dir():
+                if file_path != my_local_dir:
+                    print(file_path)
+                    raise ValueError('Somehow we got a new directory back!')
+            elif not file_path.is_file():  # time to download!
+                # check for file type if asked
+                suffix_len = len(specific_file_type)
+                type_valid = False
+                if not is_specific_file_type:
+                    type_valid = True
+                elif f'{obj.key}'[-suffix_len:] == specific_file_type:
+                    type_valid = True
+                if type_valid:
+                    download_time = time.time()
+                    bucket.download_file(
+                        obj.key, file_path
+                    )
+                    sources_download = {
+                        "Filename": str(file_path),
+                        "Source": str(obj.key),
+                        "Access Time": download_time
+                    }
+                    downloads_list.append(sources_download)
+        if len(downloads_list) > 0:
+            # save downloads itinerary
+            downloads_cols = ['Filename', 'Source', 'Access Time']
+            temp_dict = {
+                "Filename": [],
+                "Source": [],
+                "Access Time": []
+            }
+            for increment in downloads_list:
+                for key in downloads_cols:
+                    temp_dict[key].append(increment[key])
+            additions = pd.DataFrame(temp_dict)
+            # convert seconds-counter time to pd dataframe time
+            additions['Access Time']\
+                = additions['Access Time'].astype(int).astype(
+                    'datetime64[s]'
                 )
-                sources_download = {
-                    "Filename": str(online_file_path),
-                    "Source": str(obj),
-                    "Access Time": download_time
-                }
-                downloads_list.append(sources_download)
-    return
+            downloads_path = Path('../../data_inventory.csv')
+            if downloads_path.is_file():
+                downloads_df = pd.concat(
+                    [pd.read_csv(downloads_path), additions]
+                )
+                downloads_df['Access Time']\
+                    = downloads_df['Access Time'].astype('datetime64[s]')
+            else:
+                downloads_df = additions
+            downloads_df.to_csv(downloads_path, index=False)
+        return True
 
 
 if __name__ == '__main__':
@@ -79,12 +137,20 @@ if __name__ == '__main__':
     )
     # load sources
     systems_cleaned = pd.read_csv('../../data/raw/systems_20250729.csv')
+    # drop some empty unnamed columns [coming from extra commas in the csv]
+    unnamed_columns = [
+        col_name for col_name in systems_cleaned.columns.array
+        if "Unnamed:" in col_name
+    ]
+    systems_cleaned = systems_cleaned.drop(
+        columns=unnamed_columns
+    )
     # put starting date as date type
     systems_cleaned['first_timestamp'] = pd.to_datetime(
         systems_cleaned['first_timestamp'], format='%m/%d/%Y %H:%M'
     ).astype('datetime64[s]')
     systems_cleaned.loc[:, 'first_year']\
-        = systems_cleaned.loc[:, 'first_timestamp'].dt.year
+        = systems_cleaned['first_timestamp'].dt.year
     num_sources = systems_cleaned.shape[0]
     systems_cleaned.loc[:, 'is_prize_data']\
         = pd.Series([False]*num_sources, dtype='boolean')
@@ -94,21 +160,53 @@ if __name__ == '__main__':
         = pd.Series([False]*num_sources, dtype='boolean')
     systems_cleaned.loc[:, 'has_irrad_data']\
         = pd.Series([False]*num_sources, dtype='boolean')
-    systems_cleaned.loc[:, 'has_enough_irrad_data']\
-        = pd.Series([False]*num_sources, dtype='boolean')
     systems_id_set = set(systems_cleaned['system_id'].unique())
     print("Proceeding to load data from prize data.")
-    # by manual inspection of the 5 sites in the prize data,
-    # all of them satisfy the criteria
+    # by manual inspection, there are 5 sites in the prize data,
     prize_system_ids = [2105, 2107, 7333, 9068, 9069]
     for system_id in prize_system_ids:
-        relevant_rows = systems_cleaned.loc[
-            systems_cleaned.loc[:, 'system_id'] == system_id
-        ]
-        for ind in relevant_rows.index:
-            systems_cleaned.loc[ind, 'is_prize_data'] = True
-            systems_cleaned.loc[ind, 'has_irrad_data'] = True
-            systems_cleaned.loc[ind, 'has_enough_irrad_data'] = True
+        # download the metadata
+        downloader(
+            '../../data/raw/prize-metadata/',
+            f'pvdaq/2023-solar-data-prize/{system_id}_OEDI/metadata/'
+        )
+        # load the data
+        metadata_filepath = Path(
+                '../../data/raw/prize-metadata/'
+                + f'{system_id}_system_metadata.json'
+            )
+        with open(metadata_filepath) as json_reader:
+            local_metadata = json.load(json_reader)
+            system_metrics = local_metadata['Metrics']
+            # for reasons to get into later, we override the 'first_year' line
+            first_timestamp = local_metadata['System']['first_timestamp']
+            first_year = datetime.datetime.strptime(
+                first_timestamp, "%Y-%m-%d %H:%M:%S"
+            ).year
+            # assign the data to the chart.
+            relevant_rows = systems_cleaned.loc[
+                systems_cleaned.loc[:, 'system_id'] == system_id
+            ]
+            for ind in relevant_rows.index:
+                systems_cleaned.loc[ind, 'is_prize_data'] = True
+                for key in system_metrics.keys():
+                    # Avoid Irrad vs. irrad as follows.
+                    if 'rrad' in key:
+                        systems_cleaned.loc[ind, 'has_irrad_data'] = True
+                        break
+                systems_cleaned.loc[ind, 'first_year'] = first_year
+    # Note that the metadata files include both "started_on"
+    # and "first_timestamp" properties;
+    # we manually checked that first_timestamp is more accurate.
+    # lastly, we note that 7333 is a really-fast-reporting location,
+    # and has downsampled its data in a different folder.
+    # We grab the metadata for reference
+    downloader(
+        '../../data/raw/prize-metadata/',
+        'pvdaq/2023-solar-data-prize/7333_5_min_OEDI/metadata/',
+        warn_empty=True
+    )
+
     print("Proceeding to load data from parquet data.")
     # We begin by downloading metadata.
     downloader(
@@ -154,7 +252,7 @@ if __name__ == '__main__':
     # So, it can be ignored!
 
     # We first populate the 'is_lake_parquet_data' flag
-    for system_id in parquet_metrics_set.intersect(systems_id_set):
+    for system_id in parquet_metrics_set.intersection(systems_id_set):
         # first, can definitely flag the 'is_lake_parquet_data' flag
         sys_relevant_rows = systems_cleaned.loc[
             systems_cleaned.loc[:, 'system_id'] == system_id
@@ -171,7 +269,7 @@ if __name__ == '__main__':
     ]
     parquet_metrics_irrad_set = set(metrics_with_irrad['system_id'].unique())
     # now we check for having enough data
-    for system_id in parquet_metrics_irrad_set.intersect(systems_id_set):
+    for system_id in parquet_metrics_irrad_set.intersection(systems_id_set):
         # first, can definitely flag the 'has_irrad_data' flag
         sys_relevant_rows = systems_cleaned.loc[
             systems_cleaned.loc[:, 'system_id'] == system_id
@@ -180,14 +278,11 @@ if __name__ == '__main__':
             systems_cleaned.loc[ind, 'has_irrad_data'] = True
         # it was an unpleasant surprise to learn for the parquet data
         # that the first year was calculated incorrectly.
+        # (see systems 1283, 1284, 1289)
         # Our simple strategy is to start with the hinted year
         # and increment until we actually have a good starting year.
-        # We query the database, but only for the count of hits.
         first_ind = sys_relevant_rows.index[0]
         first_year = int(sys_relevant_rows.loc[first_ind, 'first_year'])
-        # As seen with sites 1283, 1284, 1289,
-        # systems start date != first Parquet data unit.
-        # start with first_year from hint and increment to right first year
         good_first_year = False
         while not good_first_year:
             prefix = "pvdaq/parquet/pvdata/"\
@@ -208,7 +303,61 @@ if __name__ == '__main__':
                 good_first_year = True
         # correct the first year
         sys_relevant_rows.loc[first_ind, 'first_year'] = first_year
-        # continue with "enough data check" tomorrow.
-        
-        
-    
+
+    print("Proceeding to load metadata from csv data.")
+    # We begin by downloading metadata.
+    downloader(
+        "../../data/raw/csv-metadata/",
+        "pvdaq/csv/system_metadata/",
+        warn_empty=False,
+        is_specific_file_type=True,
+        specific_file_type='.json'
+    )
+    downloader(
+        "../../data/raw/csv-metadata/",
+        "pvdaq/csv/system_metadata/",
+        warn_empty=False,
+        is_specific_file_type=True,
+        specific_file_type='.pdf'
+    )
+    # trouble is, this folder contains a lot of .jpgs
+    # (A satellite photo of each location.)
+    # Wonderful, but not what we are looking for.
+    # So let's drop it!
+    csv_metadata_dir = Path('../../data/raw/csv_metadata/')
+    jpgs = csv_metadata_dir.glob("*.jpg")
+    for jpg in jpgs:
+        jpg.unlink()
+    # now grab the json files, infer the system_id, and
+    # check for metadata
+    jsons = csv_metadata_dir.glob("*_system_metadata.json")
+    for file_path in jsons:
+        system_id = int(
+            f'{file_path}'.replace('_system_metadata.json', '')
+        )
+        with open(file_path) as reader:
+            local_metadata = json.load(file_path)
+            metrics = local_metadata['Metrics']
+            # we again override the 'first_year' data
+            first_timestamp = local_metadata['System']['started_on']
+            first_year = datetime.datetime.strptime(
+                first_timestamp, "%Y-%m-%d %H:%M:%S"
+            ).year
+            # assign the data to the chart.
+            relevant_rows = systems_cleaned.loc[
+                systems_cleaned.loc[:, 'system_id'] == system_id
+            ]
+            for ind in relevant_rows.index:
+                systems_cleaned.loc[ind, 'is_lake_csv_data'] = True
+                for key in metrics.keys():
+                    # Avoid Irrad vs. irrad as follows.
+                    if 'rrad' in key:
+                        systems_cleaned.loc[ind, 'has_irrad_data'] = True
+                        break
+                systems_cleaned.loc[ind, 'first_year'] = first_year
+    # finally, save the data!
+    permanent_systems_cleaned_path = Path(
+        '../../data/core/systems_cleaned.csv'
+    )
+    systems_cleaned.to_csv(permanent_systems_cleaned_path,
+                           index=False)
